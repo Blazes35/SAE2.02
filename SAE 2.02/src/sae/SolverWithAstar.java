76d9e21a56d15038f7855f24d618c9c97dd31f2c
Blazes35 <loridam2@gmail.com>
2024-04-11 18:06:23 +0200

    m

---
+++ SAE 2.02/src/sae/SolverWithAstar.java
@@ -0,0 +1,23 @@
+package sae;
+
+public class SolverWithAstar implements Solver {
+
+	@Override
+	public void resolve() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public GraphSoluce getSoluce() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getSteps() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
